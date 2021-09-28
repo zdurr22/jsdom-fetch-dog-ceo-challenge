94beadf14758f2b0199a8928ec42dba27bf1cab4
zdurr22 <zdurr22@gmail.com>
2021-09-28 15:37:26 -0400

    commit tree a4ed5ffd57a210d755412491ef24ee47166bee8f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-let images = document.getElementsByClassName("cm-string cm-string-link")
 
 function getImages() {
    return fetch(imgUrl)
@@ -11,9 +10,56 @@ function getImages() {
 }
 
 function renderImage(imageUrl) {
-    let container = document.querySelector("dog-image-container");
+    let container = document.querySelector("#dog-image-container");
     let newImg = document.createElement('img');
     newImg.src = imageUrl;
     container.appendChild(newImg);
 }
   
+function getBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        breeds = Object.keys(results.message);
+        updateBreedList(breeds);
+        addBreedSelectListener;
+     });
+ }
+
+ function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+  }
+
+  function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+
+ function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
+ function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+
+  function updateColor(event) {
+    event.target.style.color = 'silvertree'
+  }
