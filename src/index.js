e2801632dfebf5a4bfeb0c2e4becbfb9449b0c6b
zdurr22 <zdurr22@gmail.com>
2021-09-28 14:47:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let images = document.getElementsByClassName("cm-string cm-string-link")
+
+function getImages() {
+   return fetch(imgUrl)
+   .then(resp => resp.json())
+   .then(results => {
+       results.message.forEach(image=> renderImage(image))
+    });
+}
+
+function renderImage(imageUrl) {
+    let container = document.querySelector("dog-image-container");
+    let newImg = document.createElement('img');
+    newImg.src = imageUrl;
+    container.appendChild(newImg);
+}
+  
